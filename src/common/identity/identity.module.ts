d05e48d8352d2d9e9518e46bde87a2489042d990
silvante <khamidov.ko@gmail.com>
2025-06-22 19:05:38 +0500

    forgot password

--- src/common/identity/identity.module.ts
+++ src/common/identity/identity.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { IdentityController } from './identity.controller';
 import { IdentityService } from './identity.service';
+import { AppJwtModule } from 'src/jwt/jwt.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule, AppJwtModule],
   controllers: [IdentityController],
   providers: [IdentityService],
 })
