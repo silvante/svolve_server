95f79870e903e8cfc4638cf3d7880545ef05f975
silvante <khamidov.ko@gmail.com>
2025-07-26 18:18:11 +0500

    including banners

--- src/common/organizations/organizations.service.ts
+++ src/common/organizations/organizations.service.ts
@@ -48,6 +48,9 @@ export class OrganizationsService {
           },
         }),
       },
+      include: {
+        banner: true,
+      },
     });
     if (!new_organization) {
       throw new Error('Failed to create organization');
@@ -59,6 +62,9 @@ export class OrganizationsService {
     const user = req.user;
     const organization = await this.prisma.organization.findUnique({
       where: { id: id, owner_id: user.id },
+      include: {
+        banner: true,
+      },
     });
     if (!organization) {
       throw new HttpException('Organization is not defined', 404);
@@ -74,6 +80,9 @@ export class OrganizationsService {
     const user = req.user;
     const organization = await this.prisma.organization.findUnique({
       where: { unique_name: unique_name, owner_id: user.id },
+      include: {
+        banner: true,
+      },
     });
 
     // Put the payment validation logic here in the future
@@ -115,6 +124,9 @@ export class OrganizationsService {
           },
         }),
       },
+      include: {
+        banner: true,
+      },
     });
 
     if (!updated_organization) {
@@ -150,6 +162,9 @@ export class OrganizationsService {
     const updated = await this.prisma.organization.update({
       where: { id: org.id },
       data: { pincode: bcrypt.hashSync(data.new_pincode, SALT_RESULT) },
+      include: {
+        banner: true,
+      },
     });
     if (!updated) {
       throw new HttpException('internal server error', 404);
