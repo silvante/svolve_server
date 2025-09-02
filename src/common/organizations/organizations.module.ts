d2583d024cdea7f68cc9574eb1ce68ad49cffcbc
silvante <khamidov.ko@gmail.com>
2025-09-02 23:11:30 +0500

    finally startted working on stats

--- src/common/organizations/organizations.module.ts
+++ src/common/organizations/organizations.module.ts
@@ -8,9 +8,10 @@ import { JobsModule } from 'src/jobs/jobs.module';
 import { TypesModule } from './types/types.module';
 import { ClientsModule } from './clients/clients.module';
 import { WorkersModule } from './workers/workers.module';
+import { StatsModule } from './stats/stats.module';
 
 @Module({
-  imports: [PrismaModule, GlobalModule, AppJwtModule, JobsModule, TypesModule, ClientsModule, WorkersModule],
+  imports: [PrismaModule, GlobalModule, AppJwtModule, JobsModule, TypesModule, ClientsModule, WorkersModule, StatsModule],
   controllers: [OrganizationsController],
   providers: [OrganizationsService],
 })
