6c1f421f7638b2432eb1b86c825b8eaa89846abe
silvante <khamidov.ko@gmail.com>
2025-09-04 22:46:21 +0500

    stats pls

--- src/common/organizations/stats/stats.controller.ts
+++ src/common/organizations/stats/stats.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { StatsService } from './stats.service';
+import { AuthGuard } from 'src/guards/auth/auth.guard';
+import { OrganizationAccessGuard } from 'src/guards/organization-access/organization-access.guard';
+import { OwnerAccessGuard } from 'src/guards/owner-access/owner-access.guard';
+import { RequestWithUser } from 'src/interfaces/request-with-user.interface';
 
 @Controller('organizations/:org_id/stats')
 export class StatsController {
   constructor(private readonly statsService: StatsService) {}
+
+  @UseGuards(AuthGuard, OrganizationAccessGuard, OwnerAccessGuard)
+  @Get('/clients')
+  getClientsStats(@Req() req: RequestWithUser) {
+    return this.statsService.getClientsStats(req);
+  }
 }
--- src/common/organizations/stats/stats.service.ts
+++ src/common/organizations/stats/stats.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { RequestWithUser } from 'src/interfaces/request-with-user.interface';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  getClientsStats(req: RequestWithUser) {
+    const org = req.organization;
+  }
+}
