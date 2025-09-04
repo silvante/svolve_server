903cd6a11928b1f45b878a5ca65c30004a35a17e
silvante <khamidov.ko@gmail.com>
2025-09-04 23:15:08 +0500

    stats revenue

--- src/common/organizations/stats/stats.service.ts
+++ src/common/organizations/stats/stats.service.ts
@@ -54,4 +54,51 @@ export class StatsService {
 
     return { clientsByMonth, clientsByDay, clientsByType };
   }
+
+  async getRevenueStats(orgId: number) {
+    // 1. Revenue by Type
+    const revenueByType = await this.prisma.client.groupBy({
+      by: ['type_id'],
+      _sum: { price: true },
+      where: { organization_id: orgId },
+    });
+
+    // 2. Revenue by Month (always 12 months of before current month)
+    const revenueByMonth = await this.prisma.$queryRaw<
+      { month: string; total: number }[]
+    >`
+      SELECT TO_CHAR(months.month, 'YYYY-MM') as month,
+             COALESCE(SUM(c.price), 0)::int as total
+      FROM generate_series(
+              date_trunc('month', CURRENT_DATE) - interval '11 months',
+              date_trunc('month', CURRENT_DATE),
+              interval '1 month'
+           ) as months(month)
+      LEFT JOIN "Client" c
+             ON TO_CHAR(c.created_at, 'YYYY-MM') = TO_CHAR(months.month, 'YYYY-MM')
+            AND c.organization_id = ${orgId}
+      GROUP BY months.month
+      ORDER BY months.month;
+    `;
+
+    // 3. Revenue by Day (always last 30 days, including 0s)
+    const revenueByDay = await this.prisma.$queryRaw<
+      { day: string; total: number }[]
+    >`
+    SELECT TO_CHAR(days.day, 'YYYY-MM-DD') as day,
+           COALESCE(SUM(c.price), 0)::int as total
+    FROM generate_series(
+            CURRENT_DATE - interval '29 days',
+            CURRENT_DATE,
+            interval '1 day'
+         ) as days(day)
+    LEFT JOIN "Client" c
+           ON TO_CHAR(c.created_at, 'YYYY-MM-DD') = TO_CHAR(days.day, 'YYYY-MM-DD')
+          AND c.organization_id = ${orgId}
+    GROUP BY days.day
+    ORDER BY days.day;
+  `;
+
+    return { revenueByType, revenueByMonth, revenueByDay };
+  }
 }
