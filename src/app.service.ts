6af6cf9cfc9395c8a7aeb166220d31d3924cda21
silvante <khamidov.ko@gmail.com>
2025-09-30 18:40:37 +0500

    Meaning of Svolve

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    return 'Svolve is the word came from -Solve and -Evolve!';
   }
 }
