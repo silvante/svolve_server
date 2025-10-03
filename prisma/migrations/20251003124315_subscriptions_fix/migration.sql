506bea16273f97ca68ea766dce887ce4b3fd50ff
silvante <khamidov.ko@gmail.com>
2025-10-03 17:43:39 +0500

    payments: oops

---
+++ prisma/migrations/20251003124315_subscriptions_fix/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `is_active` on the `Organization` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Organization" DROP COLUMN "is_active";
