b4ec22d081e80e23cbcde0706fd622d6843be621
silvante <khamidov.ko@gmail.com>
2025-10-12 23:18:56 +0500

    Subscriptions are complate buy still needs some testing, I will work on it tomorrow

--- src/common/subscription/subscription.service.ts
+++ src/common/subscription/subscription.service.ts
@@ -76,15 +76,36 @@ export class SubscriptionService {
 
   async Webhook(req: Request, res: Response) {
     const event = req.body;
+
     switch (event.meta.event_name) {
       case 'subscription_created':
         const org_id = Number(event.meta.custom_data.organization_id);
         if (!org_id) throw new HttpException('No organization Found', 404);
-        const submition = await this.prisma.organization.update({
+        await this.prisma.organization.update({
           where: { id: org_id },
           data: {
             subscribed_at: new Date(),
-            renews_at: event.data.attributes
+            renews_at: event.data.attributes.renews_at,
+          },
+        });
+        break;
+      case 'subscription_payment_success':
+        const ORG_ID = Number(event.meta.custom_data.organization_id);
+        if (!ORG_ID) throw new HttpException('No organization Found', 404);
+        await this.prisma.organization.update({
+          where: { id: ORG_ID },
+          data: {
+            subscription_status: 'active',
+          },
+        });
+        break;
+      case 'subscription_expired':
+        const orgId = Number(event.meta.custom_data.organization_id);
+        if (!orgId) throw new HttpException('No organization Found', 404);
+        await this.prisma.organization.update({
+          where: { id: orgId },
+          data: {
+            subscription_status: 'expired',
           },
         });
         break;
@@ -94,210 +115,3 @@ export class SubscriptionService {
     return res.sendStatus(200);
   }
 }
-
-// {
-//   meta: {
-//     test_mode: true,
-//     event_name: 'subscription_updated',
-//     custom_data: { organization_id: '2' },
-//     webhook_id: 'cfed6a35-0138-40fc-adf7-c1ebd57b3eab'
-//   },
-//   data: {
-//     type: 'subscriptions',
-//     id: '1555455',
-//     attributes: {
-//       store_id: 223160,
-//       customer_id: 6921404,
-//       order_id: 6590604,
-//       order_item_id: 6533858,
-//       product_id: 651174,
-//       variant_id: 1022464,
-//       product_name: 'm1os',
-//       variant_name: 'Default',
-//       user_name: 'John',
-//       user_email: '82mailer@gmail.com',
-//       status: 'active',
-//       status_formatted: 'Active',
-//       card_brand: 'visa',
-//       card_last_four: '4242',
-//       payment_processor: 'stripe',
-//       pause: null,
-//       cancelled: false,
-//       trial_ends_at: null,
-//       billing_anchor: 12,
-//       first_subscription_item: [Object],
-//       urls: [Object],
-//       renews_at: '2025-11-12T16:17:19.000000Z',
-//       ends_at: null,
-//       created_at: '2025-10-12T16:17:21.000000Z',
-//       updated_at: '2025-10-12T16:17:25.000000Z',
-//       test_mode: true
-//     },
-//     relationships: {
-//       store: [Object],
-//       customer: [Object],
-//       order: [Object],
-//       'order-item': [Object],
-//       product: [Object],
-//       variant: [Object],
-//       'subscription-items': [Object],
-//       'subscription-invoices': [Object]
-//     },
-//     links: { self: 'https://api.lemonsqueezy.com/v1/subscriptions/1555455' }
-//   }
-// }
-// 1760285880155 => successfully connected to database
-// {
-//   meta: {
-//     test_mode: true,
-//     event_name: 'subscription_created',
-//     custom_data: { organization_id: '2' },
-//     webhook_id: '8d04b6e9-6770-432a-b0ed-8c97376af2ab'
-//   },
-//   data: {
-//     type: 'subscriptions',
-//     id: '1555457',
-//     attributes: {
-//       store_id: 223160,
-//       customer_id: 6921427,
-//       order_id: 6590617,
-//       order_item_id: 6533871,
-//       product_id: 651174,
-//       variant_id: 1022464,
-//       product_name: 'm1os',
-//       variant_name: 'Default',
-//       user_name: 'John',
-//       user_email: 'khamidov.ko@gmail.com',
-//       status: 'active',
-//       status_formatted: 'Active',
-//       card_brand: 'visa',
-//       card_last_four: '4242',
-//       payment_processor: 'stripe',
-//       pause: null,
-//       cancelled: false,
-//       trial_ends_at: null,
-//       billing_anchor: 12,
-//       first_subscription_item: [Object],
-//       urls: [Object],
-//       renews_at: '2025-11-12T16:19:55.000000Z',
-//       ends_at: null,
-//       created_at: '2025-10-12T16:19:57.000000Z',
-//       updated_at: '2025-10-12T16:20:03.000000Z',
-//       test_mode: true
-//     },
-//     relationships: {
-//       store: [Object],
-//       customer: [Object],
-//       order: [Object],
-//       'order-item': [Object],
-//       product: [Object],
-//       variant: [Object],
-//       'subscription-items': [Object],
-//       'subscription-invoices': [Object]
-//     },
-//     links: { self: 'https://api.lemonsqueezy.com/v1/subscriptions/1555457' }
-//   }
-// }
-// {
-//   meta: {
-//     test_mode: true,
-//     event_name: 'subscription_updated',
-//     custom_data: { organization_id: '2' },
-//     webhook_id: 'bd10b915-66e7-46b8-8321-34bb873946d7'
-//   },
-//   data: {
-//     type: 'subscriptions',
-//     id: '1555457',
-//     attributes: {
-//       store_id: 223160,
-//       customer_id: 6921427,
-//       order_id: 6590617,
-//       order_item_id: 6533871,
-//       product_id: 651174,
-//       variant_id: 1022464,
-//       product_name: 'm1os',
-//       variant_name: 'Default',
-//       user_name: 'John',
-//       user_email: 'khamidov.ko@gmail.com',
-//       status: 'active',
-//       status_formatted: 'Active',
-//       card_brand: 'visa',
-//       card_last_four: '4242',
-//       payment_processor: 'stripe',
-//       pause: null,
-//       cancelled: false,
-//       trial_ends_at: null,
-//       billing_anchor: 12,
-//       first_subscription_item: [Object],
-//       urls: [Object],
-//       renews_at: '2025-11-12T16:19:55.000000Z',
-//       ends_at: null,
-//       created_at: '2025-10-12T16:19:57.000000Z',
-//       updated_at: '2025-10-12T16:20:03.000000Z',
-//       test_mode: true
-//     },
-//     relationships: {
-//       store: [Object],
-//       customer: [Object],
-//       order: [Object],
-//       'order-item': [Object],
-//       product: [Object],
-//       variant: [Object],
-//       'subscription-items': [Object],
-//       'subscription-invoices': [Object]
-//     },
-//     links: { self: 'https://api.lemonsqueezy.com/v1/subscriptions/1555457' }
-//   }
-// }
-// {
-//   meta: {
-//     test_mode: true,
-//     event_name: 'subscription_payment_success',
-//     custom_data: { organization_id: '2' },
-//     webhook_id: '5015b6c9-778b-4274-b531-484330776dea'
-//   },
-//   data: {
-//     type: 'subscription-invoices',
-//     id: '4728259',
-//     attributes: {
-//       store_id: 223160,
-//       subscription_id: 1555457,
-//       customer_id: 6921427,
-//       user_name: 'John',
-//       user_email: 'khamidov.ko@gmail.com',
-//       billing_reason: 'initial',
-//       card_brand: 'visa',
-//       card_last_four: '4242',
-//       currency: 'UZS',
-//       currency_rate: '0.00008285',
-//       status: 'paid',
-//       status_formatted: 'Paid',
-//       refunded: false,
-//       refunded_at: null,
-//       subtotal: 9897405,
-//       discount_total: 0,
-//       tax: 0,
-//       tax_inclusive: false,
-//       total: 9897405,
-//       refunded_amount: 0,
-//       subtotal_usd: 820,
-//       discount_total_usd: 0,
-//       tax_usd: 0,
-//       total_usd: 820,
-//       refunded_amount_usd: 0,
-//       subtotal_formatted: 'UZS98,974',
-//       discount_total_formatted: 'UZS0',
-//       tax_formatted: 'UZS0',
-//       total_formatted: 'UZS98,974',
-//       refunded_amount_formatted: 'UZS0',
-//       urls: [Object],
-//       created_at: '2025-10-12T16:19:58.000000Z',
-//       updated_at: '2025-10-12T16:20:34.000000Z',
-//       test_mode: true
-//     },
-//     relationships: { store: [Object], subscription: [Object], customer: [Object] },
-//     links: {
-//       self: 'https://api.lemonsqueezy.com/v1/subscription-invoices/4728259'
-//     }
-//   }
-// }
