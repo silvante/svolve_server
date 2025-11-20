01c6a6903aeb60b4e06edab1912e4bbad409ba59
silvante <khamidov.ko@gmail.com>
2025-11-20 21:16:50 +0500

    admin guard ready

--- src/guards/admin/admin.guard.ts
+++ src/guards/admin/admin.guard.ts
@@ -1,11 +1,36 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  HttpException,
+  Injectable,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { Observable } from 'rxjs';
 
 @Injectable()
 export class AdminGuard implements CanActivate {
+  constructor(private readonly jwt: JwtService) {}
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
-    return true;
+    const req = context.switchToHttp().getRequest();
+    const authHeader = req.header('authorization');
+
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
+      return false;
+    }
+
+    const token = authHeader.split(' ')[1];
+
+    try {
+      const data = this.jwt.verify(token);
+
+      if (!data.admin_access) {
+        throw new HttpException('invalid access token', 408);
+      }
+      return true;
+    } catch (error) {
+      throw new HttpException('Authentication required', 408);
+    }
   }
 }
