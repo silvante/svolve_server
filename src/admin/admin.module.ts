86b702c27f0dc9d3ec3b477586d9d08a852d17af
silvante <khamidov.ko@gmail.com>
2025-11-20 21:27:31 +0500

    mkvip function

--- src/admin/admin.module.ts
+++ src/admin/admin.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AdminController } from './admin.controller';
 import { AdminService } from './admin.service';
 import { AppJwtModule } from 'src/jwt/jwt.module';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
-  imports: [AppJwtModule],
+  imports: [AppJwtModule, PrismaModule],
   controllers: [AdminController],
   providers: [AdminService],
 })
--- src/admin/admin.service.ts
+++ src/admin/admin.service.ts
@@ -1,9 +1,13 @@
 import { HttpException, Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AdminService {
-  constructor(private readonly jwt: JwtService) {}
+  constructor(
+    private readonly jwt: JwtService,
+    private readonly prisma: PrismaService,
+  ) {}
 
   async authenticate(password: string) {
     const ap = process.env.ADMIN_PASSWORD;
@@ -19,5 +23,39 @@ export class AdminService {
       permission: true,
       token: access_token,
     };
+  }
+
+  async ls_org(unique_name: string) {
+    return await this.prisma.organization.findUnique({
+      where: { unique_name },
+      select: {
+        id: true,
+        name: true,
+        unique_name: true,
+        description: true,
+        origin: true,
+        is_vip: true,
+        owner: {
+          select: {
+            id: true,
+            name: true,
+            username: true,
+          },
+        },
+      },
+    });
+  }
+
+  async mkvip(unique_name: string) {
+    const updated = await this.prisma.organization.update({
+      where: { unique_name },
+      data: {
+        is_vip: true,
+      },
+    });
+
+    return {
+      message: `organization with unique name '${updated.unique_name}' is VIP now!`
+    }
   }
 }
