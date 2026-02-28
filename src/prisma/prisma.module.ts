75477583c42921d307cf3c71095f03d9583cdcde
silvante <khamidov.ko@gmail.com>
2026-02-28 16:44:08 +0500

    Prisma 6

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -6,3 +6,18 @@ import { PrismaService } from './prisma.service';
   exports: [PrismaService],
 })
 export class PrismaModule {}
+
+// import "dotenv/config"
+// import { defineConfig, env } from "prisma/config"
+
+// console.log("DB URL:", process.env.DATABASE_URL)
+
+// export default defineConfig({
+//     schema: "prisma/schema.prisma",
+//     migrations: {
+//         path: "prisma/migrations",
+//     },
+//     datasource: {
+//         url: env("DATABASE_URL")
+//     }
+// })
